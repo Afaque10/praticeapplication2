3fdf8cef5659608a8e0be74245a06be9136b4d8b
Ebadparkar <ebadparkar1911@gmail.com>
2023-05-12 07:30:43 -0700

    bookingpage

---
+++ app/src/main/java/com/example/practiceproject2/linkspage.java
@@ -0,0 +1,14 @@
+package com.example.practiceproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class linkspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linkspage);
+    }
+}

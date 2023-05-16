0bc9e62589c619a6e37d30c14d0ca67b1b7d96b2
Ebadparkar <ebadparkar1911@gmail.com>
2023-05-16 06:58:00 -0700

    bookingsummary

---
+++ app/src/main/java/com/example/practiceproject2/bookinghistory.java
@@ -0,0 +1,14 @@
+package com.example.practiceproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookinghistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookinghistory);
+    }
+}
---
+++ app/src/main/java/com/example/practiceproject2/upcomingbooking.java
@@ -0,0 +1,13 @@
+package com.example.practiceproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class upcomingbooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upcomingbooking);
+    }
+}

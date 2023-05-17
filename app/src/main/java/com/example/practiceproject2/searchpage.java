6252b6410f370837ed4776e0faf59cc6df13911d
Ebadparkar <ebadparkar1911@gmail.com>
2023-05-17 07:12:43 -0700

    splashscrren passing

--- app/src/main/java/com/example/practiceproject2/searchpage.java
+++ app/src/main/java/com/example/practiceproject2/searchpage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class onboarding01 extends AppCompatActivity {
+public class searchpage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding01);
+        setContentView(R.layout.activity_searchpage);
     }
 }
--- app/src/main/java/com/example/practiceproject2/splashscreeen.java
+++ app/src/main/java/com/example/practiceproject2/splashscreeen.java
@@ -2,13 +2,33 @@ package com.example.practiceproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
 
 public class splashscreeen extends AppCompatActivity {
+
+    final Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreeen);
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent a = new Intent(getApplicationContext(), onboarding.class);
+                startActivity(a);
+            }
+        }, 1500);
+
     }
+
+
+
+
 }

afe7f712bdd26714c9e69a8b964fa64b074cd3d4
Ebadparkar <ebadparkar1911@gmail.com>
2023-05-19 06:47:02 -0700

    onboardinglogin

--- app/src/main/java/com/example/practiceproject2/onboarding.java
+++ app/src/main/java/com/example/practiceproject2/onboarding.java
@@ -2,13 +2,32 @@ package com.example.practiceproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class onboarding extends AppCompatActivity {
+
+    //object creation
+    Button onboardingbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        //object initialized from the ui element
+        onboardingbutton=(Button) findViewById(R.id.on_boarding_button);
+
+        //registering the eventlistener on the button
+        onboardingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent b = new Intent(getApplicationContext(),otppage.class);
+                startActivity(b);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/practiceproject2/splashscreeen.java
+++ app/src/main/java/com/example/practiceproject2/splashscreeen.java
@@ -3,34 +3,50 @@ package com.example.practiceproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.preference.PreferenceManager;
 
 
 public class splashscreeen extends AppCompatActivity {
 
-    final Handler handler = new Handler(Looper.getMainLooper());
+    final Handler handler = new Handler(Looper.getMainLooper());//handler object creation
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreeen);
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
 
+       // handler postdelayed method implemented
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent a = new Intent(getApplicationContext(), onboarding.class);
-                a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(a);
-                finish();
+
+                if(!previouslyStarted) {
+                    SharedPreferences.Editor edit = prefs.edit();
+                    edit.putBoolean(getString(R.string.pref_previously_started), Boolean.TRUE);
+                    edit.commit();
+                    Intent a = new Intent(getApplicationContext(), onboarding.class);//intent object creation
+                    a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//registering flags
+                    startActivity(a);//starting the intent
+                    finish();
+                }
+                else {
+                    Intent a = new Intent(getApplicationContext(), otppage.class);//intent object creation
+                    a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//registering flags
+                    startActivity(a);//starting the intent
+                    finish();
+                }
+
+
             }
         }, 1500);
 
     }
-
-
-
-
 }

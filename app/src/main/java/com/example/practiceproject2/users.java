2b510e4402decca1cabd32e101affd5bdbe0e9e0
Ebadparkar <ebadparkar1911@gmail.com>
2023-07-06 06:12:06 -0700

    listusers

---
+++ app/src/main/java/com/example/practiceproject2/users.java
@@ -0,0 +1,49 @@
+package com.example.practiceproject2;
+
+public class users {
+
+    private String Artist,DOB,Email,Gender,Name;
+
+    public users() {
+    }
+
+    public String getArtist() {
+        return Artist;
+    }
+
+    public void setArtist(String artist) {
+        Artist = artist;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
